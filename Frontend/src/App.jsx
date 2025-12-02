a878b2568868aabf7004f673fc34f2ce8f0c177d
kenenisa1 <kenenisamiesob@gmail.com>
2025-12-02 08:21:18 +0300

    Feat(Routes) : Add routes to App.jsx for navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,23 @@
- import React from 'react'
- 
- const App = () => {
+import Navbar from "./components/Navbar";
+import { Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import SignIn from './pages/SignIn';
+import SingUp from './pages/SingUp';
+
+const App = () => {
    return (
-     <div>App</div>
+     <div>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<HomePage/>} />
+            <Route path="/CreatePage" element = {<CreatePage/>} />
+            <Route path="/SignIN" element={<SignIn/>} />
+            <Route path="/SignUP" element={<SingUp/>} />
+        </Routes>
+     </div>
    )
- }
+}
  
  export default App
