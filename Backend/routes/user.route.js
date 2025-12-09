16a1fe1b9c4dcffe61c5a379b11f0ccf0daf46b0
kenenisa1 <kenenisamiesob@gmail.com>
2025-12-09 22:31:27 +0300

    Feat(routes) : Add signin route to user.route.js file

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import {createUser, getUser} from '../controllers/user.controller.js'
+import {createUser, getUser, deleteUser, signinUser} from '../controllers/user.controller.js'
 
 const route = express.Router();
 
 route.get('/', getUser);
 route.post('/signup', createUser )
+route.post('/signin', signinUser)
+route.delete('/:id', deleteUser)
 
 
 
