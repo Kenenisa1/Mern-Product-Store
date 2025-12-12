48875960ab77879a4a98d1726c9b0aeed3d0255d
kenenisa1 <kenenisamiesob@gmail.com>
2025-12-12 09:02:27 +0300

    Feat(source) : Add Products page to App.jsx file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,10 @@ import { Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CreatePage from './pages/CreatePage';
 import SignIn from './pages/SignIn';
-import SingUp from './pages/SingUp';
+import SignUp from './pages/SignUp';
 import {Toaster } from 'react-hot-toast';
+import Products from "./components/products";
+import Footer from "./components/Footer";
 const App = () => {
    return (
      <div>
@@ -18,9 +20,11 @@ const App = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/home" element={<HomePage/>} />
             <Route path="/CreatePage" element = {<CreatePage/>} />
-            <Route path="/SignIN" element={<SignIn/>} />
-            <Route path="/SignUP" element={<SingUp/>} />
+            <Route path="/Signin" element={<SignIn/>} />
+            <Route path="/SignuP" element={<SignUp/>} />
+            <Route path="/Products" element={<Products/>} />
         </Routes>
+        <Footer/>
      </div>
    )
 }
