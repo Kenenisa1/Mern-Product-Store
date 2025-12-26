3365615d122c9b476d9f2a5c976c27d755fc432e
kenenisa1 <kenenisamiesob@gmail.com>
2025-12-26 23:07:05 +0300

    refactor(server): Implement file uploading system in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,23 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js"; 
 import userRoutes from './routes/user.route.js'
+import path from 'path';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 
+// CORS middleware
+app.use(cors({
+  origin: 'http://localhost:5000',
+  credentials: true
+}))
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+const _dirname = path.resolve();
+app.use('/uploads', express.static(path.join(_dirname, '/uploads')));
 
 app.use("/api/products", productRoutes);
 app.use('/api/users', userRoutes)
